19d38e2abcbda2c68bcd715b7dc6be1b60d4b74c
NotChiefOfficial <NotChiefOficial@gmail.com>
2021-06-14 13:58:16 +0500

    Made Exercise 4

---
+++ code/ExerciseFour.java
@@ -0,0 +1,13 @@
+package code;
+
+/* Fourth Exercise */
+
+public class ExerciseFour {
+    public ExerciseFour() {
+        System.out.println(
+                "a: " + (-5 + 8 * 6) + "\n" +
+                "b: " + ((55+9) % 9) + "\n" +
+                "c: " + (20 + -3 * 5 / 8) + "\n" +
+                "d: " + (5 + 15 / 3 * 2 - 8 % 3));
+    }
+}
