47a91d8b9e2fc66ab5b8b3a089c497f682aca21a
NotChiefOfficial <NotChiefOficial@gmail.com>
2021-06-14 20:51:32 +0500

    Made Exercise 12

---
+++ code/Exercise12.java
@@ -0,0 +1,39 @@
+package code;
+
+import java.util.Scanner;
+
+import static java.lang.Float.parseFloat;
+
+public class Exercise12 {
+
+    float a, b, c;
+    Scanner scanner;
+
+    public Exercise12() {
+        scanner = new Scanner(System.in);
+
+        while (a <= 0) {
+            System.out.print("Input First Number: ");
+            a = getInputFloat();
+        }
+        while (b <= 0) {
+            System.out.print("Input Second Number: ");
+            b = getInputFloat();
+        }
+        while (c <= 0) {
+            System.out.print("Input Third Number: ");
+            c = getInputFloat();
+        }
+
+        System.out.println("\nAverage of these numbers is: " + (a+b+c/3));
+    }
+
+    public float getInputFloat() {
+        try {
+            return parseFloat(scanner.next());
+        } catch (Exception e) {
+            System.out.println("Type number please!");
+            return -1;
+        }
+    }
+}
