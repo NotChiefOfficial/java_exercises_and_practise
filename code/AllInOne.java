0b41f370532e9e2825fe18e376f03ae1cd66b9d4
NotChiefOfficial <NotChiefOficial@gmail.com>
2021-06-14 14:09:17 +0500

    Made Exercise 6

---
+++ code/AllInOne.java
@@ -0,0 +1,12 @@
+package code;
+
+public class AllInOne {
+    public  AllInOne(int a, int b) {
+        System.out.println(
+                "+ operation: " + (a + b) + "\n" +
+                "- operation: " + (a - b) + "\n" +
+                "* operation: " + (a * b) + "\n" +
+                "/ operation: " + (a / b) + "\n" +
+                "mod(%) operation: " + (a % b));
+    }
+}
