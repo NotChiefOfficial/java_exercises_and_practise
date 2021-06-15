811b856962f7566cc824b23d337bb04e53bfdbd0
NotChiefOfficial <NotChiefOficial@gmail.com>
2021-06-15 15:11:44 +0500

    Made Exercise 16

---
+++ code/Exercise16.java
@@ -0,0 +1,12 @@
+package code;
+
+public class Exercise16 {
+    public Exercise16() {
+        System.out.println(
+                " +=====+ \n" +
+                "[| 0 0 |]\n" +
+                " |  ^  | \n" +
+                " | '-' | \n" +
+                " +-----+ \n");
+    }
+}
